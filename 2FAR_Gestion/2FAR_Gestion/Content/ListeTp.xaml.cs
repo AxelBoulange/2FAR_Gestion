c938b372d67f67b8c2cb4070bb187dbbe3fae3a7
Ypo <121876885+AxelBoulange@users.noreply.github.com>
2023-10-02 20:07:13 +0200

    same

--- 2FAR_Gestion/2FAR_Gestion/Content/ListeTp.xaml.cs
+++ 2FAR_Gestion/2FAR_Gestion/Content/ListeTp.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,6 +19,23 @@ namespace _2FAR_Gestion
 {
     public class TP
     {
+        public int idTP {  get; set; }
+        public string nomTP { get; set; }
+        public string descriptionTP { get; set; }
+
+        List<Tache> listTaches = new List<Tache>();
+
+        public TP(int idtp, string nomtp, string descriptiontp, List<Tache> listtaches)
+        {
+            this.idTP = idtp;
+            this.nomTP = nomtp;
+            this.descriptionTP = descriptiontp;
+            this.listTaches = listtaches;
+        }
+    }
+    public class Tache
+    {
+        public int idTache { get; set; }
         public bool isCheckpoint {  get; set; }
         public bool isActif {  get; set; }
         public bool isBonus { get; set; }
@@ -27,8 +45,9 @@ namespace _2FAR_Gestion
         public int etatTache { get; set; }
 
 
-        public TP(bool ischeckpoint, bool isactif, bool isbonus, string descriptiontache,int ordretache,int pointtache, int etattache) 
+        public Tache(int idtache,bool ischeckpoint, bool isactif, bool isbonus, string descriptiontache,int ordretache,int pointtache, int etattache) 
         { 
+            this.idTache= idtache;
             this.isCheckpoint = ischeckpoint;
             this.isActif = isactif;
             this.isBonus = isbonus;
@@ -40,12 +59,34 @@ namespace _2FAR_Gestion
     }
     public partial class ListeTp : Page
     {
-        List<TP> tpList = new List<TP>();
+        List<Tache> tacheListTp1 = new List<Tache>();
+        List<Tache> tacheListTp2 = new List<Tache>();
+        List<Tache> tacheListTp3 = new List<Tache>();
+
+        List<TP> TPs = new List<TP>();
+
         public ListeTp()
         {
-            tpList.Add(new TP(false, true, false, "first", 1, 5, 0));
-            tpList.Add(new TP(true, true, false, "second", 2, 15, 0));
-            tpList.Add(new TP(false, false, true, "third", 3, 5, 0));
+            tacheListTp1.Add(new Tache(1, false, true, false, "first TP1", 1, 5, 0));
+            tacheListTp1.Add(new Tache(2, true, true, false, "second TP1", 2, 15, 0));
+            tacheListTp1.Add(new Tache(3, false, false, true, "third TP1", 3, 5, 0));
+
+            tacheListTp2.Add(new Tache(1, false, true, false, "first TP2", 1, 5, 0));
+            tacheListTp2.Add(new Tache(2, true, true, false, "second TP2", 2, 15, 0));
+            tacheListTp2.Add(new Tache(3, false, false, true, "third TP2", 3, 5, 0));
+
+            tacheListTp3.Add(new Tache(1, false, true, false, "first TP3", 1, 5, 0));
+            tacheListTp3.Add(new Tache(2, true, true, false, "second TP3", 2, 15, 0));
+            tacheListTp3.Add(new Tache(3, false, false, true, "third TP3", 3, 5, 0));
+
+
+
+            TPs.Add(new TP(1, "TP1", "Description du Tp numero 1", tacheListTp1));
+            TPs.Add(new TP(2, "TP2", "Description du Tp numero 2", tacheListTp2));
+            TPs.Add(new TP(3, "TP2", "Description du Tp numero 3", tacheListTp3));
+
+
+
             InitializeComponent();
             content.Children.Add(new NavBar());
         }
