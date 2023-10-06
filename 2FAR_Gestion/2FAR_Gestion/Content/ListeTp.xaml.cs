d4f2c0beb5113a25552923e8b640ff821d37bfe5
Ypo <121876885+AxelBoulange@users.noreply.github.com>
2023-10-06 11:16:51 +0200

    Ordre Requettes bdd
    -Promo
    -Utilisateur
    -TP
    -Tache
    -Valider
    -EtreAttribuer

--- 2FAR_Gestion/2FAR_Gestion/Content/ListeTp.xaml.cs
+++ 2FAR_Gestion/2FAR_Gestion/Content/ListeTp.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using _2FAR_Gestion.classes;
 using _2FAR_Library;
 
 namespace _2FAR_Gestion
