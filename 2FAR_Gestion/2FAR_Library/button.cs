ff4a58deaddfc0c498b6c3b71d35289322b44e48
Faydon7 <fabio.robardet@outlook.com>
2023-10-06 11:01:56 +0200

    navbar transmorphisme

---
+++ 2FAR_Gestion/2FAR_Library/button.cs
@@ -0,0 +1,50 @@
+﻿using MahApps.Metro.IconPacks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace _2FAR_Library
+{
+    internal class Button_icon : Button
+    {
+        public Button_icon(string name, int SizeWidth, int SizeHeight, Action click)
+        {
+            this.Width = SizeWidth;
+            this.Height = SizeHeight;
+
+            this.Content = new PackIconModern
+            {
+                Kind = (PackIconModernKind)Enum.Parse(typeof(PackIconModernKind), name),
+                Height = SizeHeight / 1.75,
+                Width = SizeWidth / 1.75,
+            };
+            this.BorderThickness = new Thickness(0);
+            this.Foreground = Brushes.White;
+            this.Background = Brushes.Transparent;
+            this.FontSize = SizeHeight;
+            this.Margin = new Thickness(0, 20, 0, 0);
+            this.Click += (sender, e) => click.Invoke();
+        }
+    }
+
+    public class Btn : Button
+    {
+        public Btn(string name)
+        {
+            HorizontalAlignment = System.Windows.HorizontalAlignment.Right;
+            VerticalAlignment = System.Windows.VerticalAlignment.Center;
+            this.Margin = new Thickness(0, 0, 20, 0);
+            this.Content = name;
+            this.Width = 80;
+            this.Height = 25;
+            this.Background = new System.Windows.Media.SolidColorBrush((System.Windows.Media.Color)System.Windows.Media.ColorConverter.ConvertFromString("#5e17eb"));
+
+        }
+    }
+
+}
