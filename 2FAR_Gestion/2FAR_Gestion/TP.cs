c1f421211805f24eeb293064ffd6b1fd799010f9
Faydon7 <fabio.robardet@outlook.com>
2023-10-06 08:34:31 +0200

    bidirectionnalité

--- 2FAR_Gestion/2FAR_Gestion/TP.cs
+++ 2FAR_Gestion/2FAR_Gestion/TP.cs
@@ -8,7 +8,7 @@ namespace _2FAR_Gestion
 {
     public class TP
     {
-        List<Tache> tacheList {  get; set; }
+       public List<Tache> tacheList {  get; set; }
         public int idTP { get; set; }
         public string nomTP { get; set; }
         public string descriptionTP { get; set; }
