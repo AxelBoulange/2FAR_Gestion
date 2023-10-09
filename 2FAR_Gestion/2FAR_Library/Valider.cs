7453ed3054d61fba189813c2cc28192d0b830e9d
RbFrg <144770619+RbFrg@users.noreply.github.com>
2023-10-09 09:21:21 +0200

    bdd

---
+++ 2FAR_Gestion/2FAR_Library/Valider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2FAR_Library
+{
+    public class Valider
+    {
+        public Tache tacheValider {  get; set; }
+        public Utilisateur utilisateurValider { get; set; }
+        public string reponse { get; set; }
+        public bool isJuste { get; set; }
+        public Valider(Tache tachevalider, Utilisateur utilisateurvalider, string reponsee, bool isjuste)
+        {
+            this.tacheValider = tachevalider;
+            this.utilisateurValider = utilisateurvalider;
+            this.reponse = reponsee;
+            this.isJuste = isjuste;
+        }
+    }
+}
