f95587717d51cd827ea658f5a6a84ee14ea07b74
Faydon7 <fabio.robardet@outlook.com>
2023-10-13 08:38:18 +0200

    nullable library

--- 2FAR_Gestion/2FAR_Library/Promo.cs
+++ 2FAR_Gestion/2FAR_Library/Promo.cs
@@ -8,13 +8,13 @@ namespace _2FAR_Library
 {
     public class Promo
     {
-        List<Utilisateur> utilisateurList { get; set; } 
+        List<Utilisateur>? utilisateurList { get; set; } 
         public int idPromo { get; set; }
         public string nomPromo { get; set; }
 
         public Promo(List<Utilisateur> userlist, int idpromo, string nompromo)
         {
-            this.utilisateurList = userlist;
+            this.utilisateurList = null;
             this.idPromo = idpromo;
             this.nomPromo = nompromo;
         }
