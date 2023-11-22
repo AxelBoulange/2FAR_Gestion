ffc9bbc97c671b0df2e2e832095749a5856e708a
Ypo <121876885+AxelBoulange@users.noreply.github.com>
2023-11-22 09:51:42 +0100

    Affichage des hover dans la navbar

---
+++ 2FAR_Gestion/2FAR_Gestion/Content/TP/ListeTachesTP.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace _2FAR_Gestion.Content.TP;
+
+public partial class ListeTachesTP : Page
+{
+    public ListeTachesTP()
+    {
+        InitializeComponent();
+    }
+}
