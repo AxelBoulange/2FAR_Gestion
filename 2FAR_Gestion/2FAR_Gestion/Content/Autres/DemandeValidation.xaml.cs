996c4df0ec437385954390e2e2de1f96118486ce
Ypo <121876885+AxelBoulange@users.noreply.github.com>
2023-11-22 11:46:21 +0100

    git ignore Fonctionel (normalement)

---
+++ 2FAR_Gestion/2FAR_Gestion/Content/Autres/DemandeValidation.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace _2FAR_Gestion.Content.Autres;
+
+public partial class DemandeValidation : Page
+{
+    public DemandeValidation()
+    {
+        InitializeComponent();
+    }
+}
