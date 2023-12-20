37511d3153e899f34a72122fbed749d2e64986ac
Ypo <121876885+AxelBoulange@users.noreply.github.com>
2023-12-20 17:05:26 +0100

    DemandeValidationTache FINIT

--- 2FAR_Gestion/2FAR_Gestion/Content/Tache/DemandeValidationTache.xaml.cs
+++ 2FAR_Gestion/2FAR_Gestion/Content/Tache/DemandeValidationTache.xaml.cs
@@ -10,31 +10,41 @@ namespace _2FAR_Gestion
 
     public partial class DemandeValidation
     {
+        public Dictionary<string,Action<object, EventArgs>> actionsButton { get; set; }
         public DemandeValidation()
         {
-            //regardes les attentes de validation et les valider qui ont isJuste = false
-            
+            actionsButton = new Dictionary<string, Action<object, EventArgs>>() { { "Valider", Valider }, { "Rejeter", Rejeter } };
             InitializeComponent();
-            Dictionary<string,Action<object, EventArgs>> actionsButton = new Dictionary<string,Action<object, EventArgs>>() { {"Valider",Valider},{"Rejeter",Rejeter}};
-            foreach (var attenteValidation in Ados.listeAttenteValidations)
-            {
-                listCartes.Children.Add(new Carte(attenteValidation.utilisateur.nomUtilisateur + "\n" + attenteValidation.utilisateur.nomPromo, attenteValidation.tache.titreTache, actionsButton, 25, 20, attenteValidation){Margin = new Thickness(20,10,20,10)});
-            }
+            AffichageListe();
         }
         public void Valider(object o, EventArgs e)
         {
             if (o is _2FAR_Library.Graphique.Btn b && b.Parent is StackPanel st && st.Parent is Grid g && g.Parent is Carte c && c.objectCarte is _2FAR_Library.AttendreValidation attendreValidation)
             {
-                
+                ActionQuandBouttonClicke(attendreValidation,"Tache Validé");
             }
         }
         public void Rejeter(object o, EventArgs e)
         {
             if (o is _2FAR_Library.Graphique.Btn b && b.Parent is StackPanel st && st.Parent is Grid g && g.Parent is Carte c && c.objectCarte is _2FAR_Library.AttendreValidation attendreValidation)
             {
-                
+                ActionQuandBouttonClicke(attendreValidation, "Non Validé");
             }
         }
 
+        private void ActionQuandBouttonClicke(_2FAR_Library.AttendreValidation attendreValidation, string reponsse)
+        {
+            Ados.listeValidations.Add(new Valider(attendreValidation.tache, attendreValidation.utilisateur, reponsse, true));
+            Ados.listeAttenteValidations.Remove(Ados.listeAttenteValidations.Where(atV => atV.utilisateur.idUtilisateur == attendreValidation.utilisateur.idUtilisateur && atV.tache.idTache == attendreValidation.tache.idTache).First());
+            AffichageListe();
+        }
+        private void AffichageListe()
+        {
+            listCartes.Children.Clear();
+            foreach (var attenteValidation in Ados.listeAttenteValidations)
+            {
+                listCartes.Children.Add(new Carte(attenteValidation.utilisateur.nomUtilisateur + "\n" + attenteValidation.utilisateur.nomPromo, attenteValidation.tache.titreTache, actionsButton, 25, 20, attenteValidation){Margin = new Thickness(20,10,20,10)});
+            }
+        }
     }
 }
